55f89a7ffc741c8ffe380017d6df4ad4b57f2bc3
ma-Rac <mno.racca@gmail.com>
2020-08-28 13:27:23 +0200

    initialized lein reagent template

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/tinyquiz.jar clojure.main -m tinyquiz.server
---
+++ env/prod/clj/tinyquiz/middleware.clj
@@ -0,0 +1,6 @@
+(ns tinyquiz.middleware
+  (:require
+   [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+
+(def middleware
+  [#(wrap-defaults % site-defaults)])
